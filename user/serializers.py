ddd252a04253c2ff313b2b36aaf14617ae347378
Nick Konkov <kharkivnick@gmail.com>
2026-01-19 00:07:40 +0200

    Solution_3

--- user/serializers.py
+++ user/serializers.py
@@ -19,6 +19,9 @@ class UserSerializer(serializers.ModelSerializer):
         """Update user with encrypted password"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
-        user.set_password(password)
-        user.save()
+
+        if password:
+            user.set_password(password)
+            user.save()
+
         return user
